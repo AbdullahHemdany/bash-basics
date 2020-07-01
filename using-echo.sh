9ae0e67b028b589bc2708495341060e0e93a8f82
abdullahhemdany <abdullahhemdany@outlook.com>
2020-07-01 20:03:31 +0200

    adding using-echo.sh to master

---
+++ using-echo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# author : abdullah hemdany
+# date : 1/7/2020
+# using echo
+
+echo "Printing text on screen"
+echo -n "Printing text without newline" ; echo " hey"
+echo -e "\nRemoving \t special \t characters\n"
