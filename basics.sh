b2a7aec7c7ed470a6a35dee6cbdf060a3ceb7bf3
abdullahhemdany <abdullahhemdany@outlook.com>
2020-07-01 02:33:21 +0200

    added comments to code

--- basics.sh
+++ basics.sh
@@ -5,19 +5,31 @@
 
 # start of the script
 echo "start bash script."
-
 echo "--- --- --- --- --- --- ---"
 
+# --- --- --- Hello World in Bash --- --- ---
 # echo command : echo something into screen
+
 # echo "Hello World!"
 
-# Variables
-NAME="Abdullah Hemdany"
-AGE=29
+# --- --- --- --- Variables --- --- --- --- 
+# How to Define variables , VARIABLE_NAME=VARIABLE_VALUE
+# to echo variables use "echo "Hello , $VARIABLE_NAME""
+
+# NAME="Abdullah Hemdany"
+# AGE=29
+# echo "My name is ${NAME}, I am ${AGE} years old"
+
+# --- --- --- Read Input From User --- --- ---
+# usage: read -p "MESSAGE TO USER" VARIABLE_NAME
+read -p "Enter your name, " NAME
+read -p "Enter your age, " AGE
+
+echo "Basic info"
+echo "Name: ${NAME}"
+echo "Age: ${AGE}"
 
 
-
-echo "my name is $NAME, I am $AGE years old"
 
 # end of script
 echo "--- --- --- --- --- --- ---"
