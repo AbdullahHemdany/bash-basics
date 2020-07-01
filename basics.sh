bb1d962edc2bd3e2679fd867544fcce26d312b49
abdullahhemdany <abdullahhemdany@outlook.com>
2020-07-01 02:21:21 +0200

    added variables

--- basics.sh
+++ basics.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-echo "start bash script"
+# author : Abdullah Hemdany
+# date : 1 / 7 / 2020
 
+# start of the script
+echo "start bash script."
+
+echo "--- --- --- --- --- --- ---"
+
+# echo command : echo something into screen
+# echo "Hello World!"
+
+# Variables
+NAME="Abdullah Hemdany"
+AGE=29
+
+
+
+echo "my name is $NAME, I am $AGE years old"
+
+# end of script
+echo "--- --- --- --- --- --- ---"
 echo "end bash script"
